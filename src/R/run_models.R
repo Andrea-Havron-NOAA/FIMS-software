cb1d8feaaa1fc825444f9ffc8f2ec96033c8d472
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2021-08-09 16:56:12 -0700

    modify code to use runSTAN and set up MCMC comparison code

--- src/R/run_models.R
+++ src/R/run_models.R
@@ -2,150 +2,104 @@ library(rstan)
 library(TMB)
 library(tmbstan)
 library(cmdstanr)
+library(profvis)
 
 source('data/simdata_logistic.R')
 source('src/R/utils.R')
 source('src/R/model_setup.R')
 
-gompertz.results <- list()
-logistic.results <- list()
-
-
-logistic.init <- function(){
-  list(theta = c(log(0.5), log(80)), ln_sig=-1,ln_tau=-1,
-       u = rep(1,n))
+#Implement simulation for n = 2^seq(5,11,1)
+n.seq <- seq(5,11,1)
+for(i in 1:length(n.seq)){
+  n <- 2^n.seq[i]
+  gompertz.results <- list()
+  logistic.results <- list()
+  
+  gompertz.init <- function(){
+    list(theta = c(0,0), ln_sig = 0,
+         ln_tau = 0,
+         u = rep(0,n))
+  }
+  
+  
+  #run simulation models
+  #gompertz
+  Mod <- 'gompertz'
+  simdata <- gendat(seed=123,
+                    N=n,
+                    theta = c(2,0.8),
+                    u1 = 4,
+                    var = list(proc=0.1,obs=0.5),
+                    mod.name = Mod)
+  write.csv(data.frame(y=simdata), file = paste0('data/gompertz/gompertz', '_n', n, '.csv'))
+  results <- runTMB(simdata,Mod)
+  
+  #modify and save for Julia
+  inits <- c(alpha = unname(results$inits[1]), beta = unname(results$inits[2]), sigma = unname(exp(results$init[3])), 
+             tau = unname(exp(results$inits[4])), u_init = unname(results$inits[5]), 
+             results$inits[6:length(results$inits)])
+  save(inits, file = paste0('data/gompertz/gompertzInits', '_n', n, '.RData'))
+  gompertz.results <- list(tmb = results$tmb, tmbstan = results$tmbstan)
+  
+  # Init functions
+  gompertz.init <- function(){
+    list(theta = results$inits[1:2],
+         ln_sig = results$inits[3],
+         ln_tau = results$inits[4],
+         u = results$inits[5:length(results$inits)])
+  }
+  
+  #stan
+  #use improper priors to compare with tmbstan
+  gompertz.results$stanP0 <- runSTAN(simdata, Mod,0)
+  #use vague priors
+  gompertz.results$stanP1 <- runSTAN(simdata, Mod,1)
+  
+  save(gompertz.results, file = paste0('results/gompertz/gompertz', '_n', n, '.RData'))
+  
+  #Compare stan, tmbstan, tmb
+  cbind(true=c(2,0.8,0.1,0.5),sapply(gompertz.results, function(x) x$par.est))
+  sapply(gompertz.results, function(x) x$se.est)
+  sapply(gompertz.results, function(x) x$time)
+  sapply(gompertz.results, function(x) x$meanESS)
+  sapply(gompertz.results, function(x) x$minESS)
+  
+  
+  #logistic model
+  Mod <- 'logistic'
+  
+  logistic.init <- function(){
+    list(theta = c(log(0.5), log(80)), ln_sig=-1,ln_tau=-1,
+         u = rep(1,n))
+  }
+  
+  simdata <- gendat(seed=123,
+                    N=n,
+                    theta = c(0.2,100),
+                    u1 = 4,
+                    var = list(proc=0.01,obs=0.001),
+                    mod.name = Mod)
+  write.csv(data.frame(y=simdata), file = paste0('data/logistic/logistic', '_n', n, '.csv'))
+  results <- runTMB(simdata,Mod) 
+  #modify and save for Julia
+  inits <- c(r = unname(exp(results$inits[1])), K = unname(exp(results$inits[2])), sigma = unname(exp(results$init[3])), 
+             tau = unname(exp(results$inits[4])), u_init = unname(results$inits[5]), 
+             results$inits[6:length(results$inits)])
+  save(inits, file = paste0('data/logistic/logisticInits', '_n', n, '.RData'))
+  logistic.results <-  list(tmb = results$tmb, tmbstan = results$tmbstan)
+  
+  
+  #stan
+  #use improper priors to compare with tmbstan
+  logistic.results$stanP0 <- runSTAN(simdata, Mod,0)
+  #use vague priors
+  logistic.results$stanP1 <- runSTAN(simdata, Mod,1)
+  
+  save(logistic.results, file = paste0('results/logistic/logistic', '_n', n, '.RData'))
+  #Compare rstan, tmbstan, tmb
+  cbind(true=c(0.2,100,0.01,0.001),round(sapply(logistic.results, function(x) x$par.est),3))
+  sapply(logistic.results, function(x) x$se.est)
+  sapply(logistic.results, function(x) x$time)
+  sapply(logistic.results, function(x) x$meanESS)
+  sapply(logistic.results, function(x) x$minESS)
 }
-
-n <- 100
-#run simulation models
-#gompertz
-Mod <- 'gompertz'
-simdata <- gendat(seed=123,
-                  N=n,
-                  theta = c(2,0.8),
-                  u1 = 4,
-                  var = list(proc=0.1,obs=0.5),
-                  mod.name = Mod)
-write.csv(data.frame(y=simdata), file = 'data/gompertz.csv')
-results <- runTMB(simdata,Mod)
-gompertz.results <- list(tmb = results$tmb, tmbstan = results$tmbstan)
-
-# Init functions
-gompertz.init <- function(){
-  list(theta = results$inits[1:2],
-       ln_sig = results$inits[3],
-       ln_tau = results$inits[4],
-       u = results$inits[5:length(results$inits)])
-}
-
-#stan
-#use improper priors to compare with tmbstan
-a <- Sys.time()
-hyperParameters <- list(
-  hyperSig = c(0,0), #dgamma
-  hyperTau = c(0,0), #dgamma
-  hyperTheta1 = 0, #normal mean
-  hyperTheta2 = 0 #normal sd
-)
-Dat <- mkSTANdat(simdata, hyperParameters) 
-file <- file.path('src', 'stan', 'gompertz.stan')
-mod <- cmdstan_model(file) #compile stan model into C++
-fit.p0 <- mod$sample(
-  data = Dat,
-  init = gompertz.init,
-  iter_warmup = 2000, iter_sampling = 2000
-)
-
-b <- Sys.time()
-gompertz.results$stanP0 <- list(par.est = as.vector(as.matrix(fit.p0$summary(c('theta', 'ln_sig', 'ln_tau'), 'mean')[,2])),
-                                se.est = as.vector(as.matrix(fit.p0$summary(c('theta', 'ln_sig', 'ln_tau'), 'sd')[,2])),
-                                time = difftime(b,a, units = 'mins'))
-gompertz.results$stanP0$par.est[3:4] <- exp(gompertz.results$stanP0$par.est[3:4])
-#use proper vague priors
-a <- Sys.time()
-hyperParameters <- list(
-  hyperSig = c(0.001,0.001), #dinvgamma
-  hyperTau = c(0.001,0.001), #dinvgamma
-  hyperTheta1 = 0, #normal mean
-  hyperTheta2 = 100 #normal sd
-)
-Dat <- mkSTANdat(simdata, hyperParameters)
-Dat$prior_type <- 1
-fit.p1 <- mod$sample(
-  data = Dat,
-  init = gompertz.init,
-  iter_warmup = 2000, iter_sampling = 2000
-)
-b <- Sys.time()
-gompertz.results$stanP1 <-  list(par.est = as.vector(as.matrix(fit.p1$summary(c('theta', 'ln_sig', 'ln_tau'), 'mean')[,2])),
-                                 se.est = as.vector(as.matrix(fit.p1$summary(c('theta', 'ln_sig', 'ln_tau'), 'sd')[,2])),
-                                 time = difftime(b,a, units = 'mins'))
-gompertz.results$stanP1$par.est[3:4] <- exp(gompertz.results$stanP1$par.est[3:4])
-save(gompertz.results, file = 'results/gompertz.RData')
-#Compare stan, tmbstan, tmb
-cbind(true=c(2,0.8,0.1,0.5),sapply(gompertz.results, function(x) x$par.est))
-sapply(gompertz.results, function(x) x$se.est)
-sapply(gompertz.results, function(x) x$time)
-
-
-#logistic model
-Mod <- 'logistic'
-
-simdata <- gendat(seed=123,
-                  N=100,
-                  theta = c(0.2,100),
-                  u1 = 4,
-                  var = list(proc=0.01,obs=0.001),
-                  mod.name = Mod)
-write.csv(data.frame(y=simdata), file = 'data/logistic.csv')
-results <- runTMB(simdata,Mod) 
-logistic.results <-  list(tmb = results$tmb, tmbstan = results$tmbstan)
-
-
-#stan
-#use improper priors to compare with tmbstan
-a <- Sys.time()
-hyperParameters <- list(
-  hyperSig = c(0,0), #dinvgamma
-  hyperTau = c(0,0), #dinvgamma
-  hyperTheta1 = c(0,0), #normal
-  hyperTheta2 = c(0,0) #normal 
-)
-Dat <- mkSTANdat(simdata, hyperParameters) 
-file <- file.path('src', 'stan', 'logistic.stan')
-mod <- cmdstan_model(file) #compile stan model into C++
-fit.p0 <- mod$sample(
-  data = Dat,
-  init = logistic.init,
-  iter_warmup = 2000, iter_sampling = 2000
-)
-b <- Sys.time()
-logistic.results$stanP0 <-  list(par.est = as.vector(as.matrix(fit.p0$summary(c('r', 'K', 'sigma', 'tau'), 'mean')[,2])),
-                                 se.est = as.vector(as.matrix(fit.p0$summary(c('r', 'K', 'sigma', 'tau'), 'sd')[,2])),
-                                 time = difftime(b,a, units = 'mins'))
-
-#use proper vague priors
-a <- Sys.time()
-hyperParameters <- list(
-  hyperSig = c(0.001,0.001), #dinvgamma
-  hyperTau = c(0.001,0.001), #dinvgamma
-  hyperTheta1 = c(-1,4), #lognormal
-  hyperTheta2 = c(5,4) #lognormal
-)
-Dat <- mkSTANdat(simdata, hyperParameters)
-Dat$prior_type <- 1
-fit.p1 <- mod$sample(
-  data = Dat,
-  init = logistic.init,
-  iter_warmup = 2000, iter_sampling = 2000
-)
-b <- Sys.time()
-logistic.results$stanP1 <- list(par.est = as.vector(as.matrix(fit.p1$summary(c('r','K', 'sigma', 'tau'), 'mean')[,2])),
-                                se.est = as.vector(as.matrix(fit.p1$summary(c('r', 'K', 'sigma', 'tau'), 'sd')[,2])),
-                                time = difftime(b,a, units = 'mins'))
-
-save(logistic.results, file = 'results/logistic.RData')
-#Compare rstan, tmbstan, tmb
-cbind(true=c(0.2,100,0.01,0.001),round(sapply(logistic.results, function(x) x$par.est),3))
-sapply(logistic.results, function(x) x$se.est)
-sapply(logistic.results, function(x) x$time)
