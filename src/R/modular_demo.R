d07c3ff4f60182084eeef9301a108c391c3972a0
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2021-08-18 12:35:05 -0700

    update initial values

--- src/R/modular_demo.R
+++ src/R/modular_demo.R
@@ -4,14 +4,13 @@ dyn.load(dynlib("src/Rcpp/logisticGrowth"))
 y <- read.csv('data/logistic/logistic_n128.csv')$y
 
 dat <- list(y=y)
-par <- list(theta = 0,
-            ln_sig = 0,
-            ln_tau = 0,
-            u = dat$y*0)
+par <- list(theta = c(log(0.5), log(80)), ln_sig=-1,ln_tau=-1,
+            u = rep(1,length(y)))
 obj <- MakeADFun(dat, par, random="u", DLL="logisticGrowth")
 newtonOption(obj, smartsearch=FALSE)
 obj$fn()
 obj$gr()
 system.time(opt <- nlminb(obj$par, obj$fn, obj$gr))
 rep <- sdreport(obj)
-rep
+exp(summary(rep, 'fixed')[,1])
+
