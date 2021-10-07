d3f6fe56e7e327d2dcb2487f3e66e24e8969f075
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2021-10-07 09:54:42 -0700

    increase sample size

--- R/modular_demo.R
+++ R/modular_demo.R
@@ -1,21 +1,23 @@
 library(TMB)
 compile("src/Rcpp/logisticGrowth.cpp", flags= "-w")
 dyn.load(dynlib("src/Rcpp/logisticGrowth"))
-simdata <- gendat(seed=123,
-                  N=100,
+y <- gendat(seed=123,
+                  N=2^12,
                   theta = c(0.2,100),
                   u1 = 4,
                   var = list(proc=0.01,obs=0.001),
                   mod.name = 'logistic')
-
 dat <- list(y=y)
 par <- list(theta = c(log(0.5), log(80)), ln_sig=-1,ln_tau=-1,
             u = rep(1,length(y)))
+a <- Sys.time()
 obj <- MakeADFun(dat, par, random="u", DLL="logisticGrowth")
 newtonOption(obj, smartsearch=FALSE)
 obj$fn()
 obj$gr()
 system.time(opt <- nlminb(obj$par, obj$fn, obj$gr))
 rep <- sdreport(obj)
+b <- Sys.time()
+time.diff(b,a)
 exp(summary(rep, 'fixed')[,1])
 
