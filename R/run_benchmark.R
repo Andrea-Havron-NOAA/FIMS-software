c1bddf270474b73f99d969edd26ce6ec3d106093
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2021-10-21 09:22:17 -0700

    add admb to benchmark

--- R/run_benchmark.R
+++ R/run_benchmark.R
@@ -8,6 +8,7 @@
 library(TMB)
 library(tmbstan)
 library(cmdstanr)
+library(R2admb)
 
 source('data/simdata.R')
 source('R/utils.R')
@@ -17,55 +18,55 @@ source('R/model_setup.R')
 n.seq <- seq(5,12,1)
 for(i in 1:length(n.seq)){
   n <- 2^n.seq[i]
-  
-  gompertz.init <- function(){
-    list(theta = c(0,0), ln_sig = 0,
-         ln_tau = 0,
-         u = rep(0,n))
-  }
-
-
-  #run simulation models
-  #gompertz
-  Mod <- 'gompertz'
-  simdata <- gendat(seed=i,
-                    N=n,
-                    theta = c(2,0.8),
-                    u1 = 4,
-                    var = list(proc=0.1,obs=0.5),
-                    mod.name = Mod)
-  write.csv(data.frame(y=simdata), file = paste0('data/gompertz/gompertz', '_n', n, '.csv'))
-  results <- runTMB(simdata,Mod)
-
-  #modify and save for Julia
-  inits <- c(alpha = unname(results$inits[1]), beta = unname(results$inits[2]), sigma = unname(exp(results$init[3])),
-             tau = unname(exp(results$inits[4])), u_init = unname(results$inits[5]),
-             results$inits[6:length(results$inits)])
-  save(inits, file = paste0('data/gompertz/gompertzInits', '_n', n, '.RData'))
-  gompertz.results <- list(tmb = results$tmb, tmbstan = results$tmbstan)
-
-  # Init functions
-  gompertz.init <- function(){
-    list(theta = results$inits[1:2],
-         ln_sig = results$inits[3],
-         ln_tau = results$inits[4],
-         u = results$inits[5:length(results$inits)])
-  }
-
-  #stan
-  # #use improper priors to compare with tmbstan
-  # gompertz.results$stanP0 <- runSTAN(simdata, Mod,0)
-  #use vague priors
-  gompertz.results$stan <- runSTAN(simdata, Mod, 1)
-
-  save(gompertz.results, file = paste0('results/gompertz/gompertz', '_n', n, '.RData'))
-
-  #Compare stan, tmbstan, tmb
-  cbind(true=c(2,0.8,0.1,0.5),sapply(gompertz.results, function(x) x$par.est))
-  sapply(gompertz.results, function(x) x$se.est)
-  sapply(gompertz.results, function(x) x$time)
-  sapply(gompertz.results, function(x) x$meanESS)
-  sapply(gompertz.results, function(x) x$minESS)
+  # 
+  # gompertz.init <- function(){
+  #   list(theta = c(0,0), ln_sig = 0,
+  #        ln_tau = 0,
+  #        u = rep(0,n))
+  # }
+  # 
+  # 
+  # #run simulation models
+  # #gompertz
+  # Mod <- 'gompertz'
+  # simdata <- gendat(seed=i,
+  #                   N=n,
+  #                   theta = c(2,0.8),
+  #                   u1 = 4,
+  #                   var = list(proc=0.1,obs=0.5),
+  #                   mod.name = Mod)
+  # write.csv(data.frame(y=simdata), file = paste0('data/gompertz/gompertz', '_n', n, '.csv'))
+  # results <- runTMB(simdata,Mod)
+  # 
+  # #modify and save for Julia
+  # inits <- c(alpha = unname(results$inits[1]), beta = unname(results$inits[2]), sigma = unname(exp(results$init[3])),
+  #            tau = unname(exp(results$inits[4])), u_init = unname(results$inits[5]),
+  #            results$inits[6:length(results$inits)])
+  # save(inits, file = paste0('data/gompertz/gompertzInits', '_n', n, '.RData'))
+  # gompertz.results <- list(tmb = results$tmb, tmbstan = results$tmbstan)
+  # 
+  # # Init functions
+  # gompertz.init <- function(){
+  #   list(theta = results$inits[1:2],
+  #        ln_sig = results$inits[3],
+  #        ln_tau = results$inits[4],
+  #        u = results$inits[5:length(results$inits)])
+  # }
+  # 
+  # #stan
+  # # #use improper priors to compare with tmbstan
+  # # gompertz.results$stanP0 <- runSTAN(simdata, Mod,0)
+  # #use vague priors
+  # gompertz.results$stan <- runSTAN(simdata, Mod, 1)
+  # 
+  # save(gompertz.results, file = paste0('results/gompertz/gompertz', '_n', n, '.RData'))
+  # 
+  # #Compare stan, tmbstan, tmb
+  # cbind(true=c(2,0.8,0.1,0.5),sapply(gompertz.results, function(x) x$par.est))
+  # sapply(gompertz.results, function(x) x$se.est)
+  # sapply(gompertz.results, function(x) x$time)
+  # sapply(gompertz.results, function(x) x$meanESS)
+  # sapply(gompertz.results, function(x) x$minESS)
 
 
   #logistic model
@@ -103,12 +104,20 @@ for(i in 1:length(n.seq)){
   logistic.results$stan <- runSTAN(simdata, Mod,1)
   
   save(logistic.results, file = paste0('results/logistic/logistic', '_n', n, '.RData'))
+  
+  
+  #admb
+  logistic.results$admb <- runADMB(simdata, Mod)
+  save(logistic.results, file = paste0('results/logistic/logistic', '_n', n, '.RData'))
+  
   #Compare rstan, tmbstan, tmb
-  cbind(true=c(0.2,100,0.01,0.001),round(sapply(logistic.results, function(x) x$par.est),3))
+  cbind(true=c(0.2,100,sqrt(0.01),sqrt(0.001)),round(sapply(logistic.results, function(x) x$par.est),3))
   sapply(logistic.results, function(x) x$se.est)
   sapply(logistic.results, function(x) x$time)
   sapply(logistic.results, function(x) x$meanESS)
   sapply(logistic.results, function(x) x$minESS)
+  
+  
   
 }
 
